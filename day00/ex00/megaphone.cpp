c2e454385fe40abf3e794087e692325a553e4ff9
Ayoub Maatouch <contact@maatouch.com>
2021-10-31 22:06:01 +0100

    1st Commit

---
+++ day00/ex00/megaphone.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cctype>
+
+int main(int argc, char* argv[])
+{
+    if (argc == 1)
+        std::cout<<"* LOUD AND UNBEARABLE FEEDBACK NOISE *"<<std::endl;
+     else
+     {   char ch;
+        for(int i = 1; i < argc; i++)
+        {
+            for (int j = 0; j < (int)strlen(argv[i]); j++) 
+            {
+                ch = toupper(argv[i][j]);
+                std::cout << ch;
+            }
+            std::cout<<" ";
+        }
+        std::cout<<std::endl;
+    }
+
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int cout (std::string x)
+{
+    std::cout << x;
+    return 0;
+}
+int    main(void)
+{
+    cout << "Huroku\n";
+    return (0);
+}
+
