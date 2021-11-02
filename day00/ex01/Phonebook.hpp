baf7f1c2fd4db6b82757366519b945498d439c65
Ayoub Maatouch <aymaatou@e1r11p8.1337.ma>
2021-11-02 13:36:19 +0100

    init class

--- day00/ex01/Phonebook.hpp
+++ day00/ex01/Phonebook.hpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include <string>
 
+
+
+class contact 
+{
+    public:
+            std::string first_name;
+            std::string last_name;
+            std::string nickname;
+            int phone_number;
+            std::string darkest_secret;
+};
+
 class Phonebook 
 {
-    private:
-
     public:
-        Phonebook(void);
-        ~Phonebook(void);
+        contact myphonebook[8];
 };
--- day00/ex01/phonebook.cpp
+++ day00/ex01/phonebook.cpp
@@ -1,31 +1,53 @@
 #include "Phonebook.hpp"
 
-Phonebook::~Phonebook(void)
+Phonebook ft_adding(Phonebook phonebook, int i)
 {
-    std::cout<<"dest Called\n";
-    return;
+    if (i < 8)
+    {
+        std::cout<<"Whats Your first name ?"<<std::endl;
+        std::cin >> phonebook.myphonebook[i].first_name;
+        std::cout<<"Whats Your last name ?"<<std::endl;
+        std::cin >> phonebook.myphonebook[i].last_name;
+        std::cout<<"Provide a nickname for you."<<std::endl;
+        std::cin >> phonebook.myphonebook[i].nickname;
+        std::cout<<"Tell me your darkest secret."<<phonebook.myphonebook[i].nickname<<std::endl;
+        std::cin >> phonebook.myphonebook[i].darkest_secret;
+    }
+    else
+        std::cout<<"Your Phonebook has reached it's Limets"<<std::endl;
+    return phonebook;
 }
 
-Phonebook::Phonebook(void)
+int ft_search(Phonebook phonebook)
 {
-    std::cout<<"Const Called\n";
-    return;
+    int i = 0;
+    while (i < 8 )
+    {
+        std::cout <<"first"<<phonebook.myphonebook[0].first_name<<std::endl;
+        std::cout <<"Last"<<phonebook.myphonebook[0].last_name<<std::endl;
+        i++;
+    }
+    
+    return EXIT_SUCCESS;
 }
 
 int main (void)
 {
-    // Phonebook inst;
+    Phonebook instance;
+    std::string cmd;
+    int i = -1;
     while (true)
-    {
-        std::string cmd;
+    {    
         std::cout<<"Enter your input"<<std::endl;
         std::cin>>cmd;
         if (cmd == "ADD")
-            ft_adding();
+              instance = ft_adding(instance, ++i);
         else if (cmd == "SEARCH")
-            ft_search();
-        else if (cmd == "EXIT")
+             ft_search(instance);
+        else if (cmd == "EXIT") 
             return EXIT_SUCCESS;
+        else
+            std::cout<<"You can ONLY use : ADD, SEARCH, EXIT."<<std::endl;
 
     }
     return 0;
