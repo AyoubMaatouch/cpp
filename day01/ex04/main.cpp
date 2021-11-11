b4bcb2d8d6ad79340b2105337887c1375c73b5ea
Ayoub Maatouch <aymaatou@e1r9p9.1337.ma>
2021-11-11 04:15:19 +0100

    Mehdi hdaya

---
+++ day01/ex04/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <fstream>
+
+int error_file(std::string std)
+{
+	std::cout<<std<<std::endl;
+	return EXIT_FAILURE;
+}
+
+bool is_empty(std::ifstream& pFile)
+{
+    return pFile.peek() == std::ifstream::traits_type::eof();
+}
+
+std::string ReplaceAll(std::string str, const std::string& s1, const std::string& s2) 
+{
+	size_t place;
+
+	if (s1 != s2)
+	{
+		while ((place = str.find(s1)) != std::string::npos)
+		{ 
+			str.erase(place, 5);
+			str.insert(place, s2);
+		}
+	}
+	return str;
+}
+
+int main (int ac, char **av) 
+{
+	if (ac == 2)
+	{
+		std::ifstream in_file;
+		std::ofstream out_file;
+		
+		std::string out = std::string(av[1]) + std::string(".replace");
+		in_file.open(av[1]);
+		
+		if (in_file.is_open() && !is_empty(in_file))
+		{
+			out_file.open(out);
+			if (!out_file.is_open())
+				return error_file("Destination File Error");
+			for( std::string line; getline( in_file, line ); )
+			{
+				out_file << ReplaceAll(line, std::string("Ayoub"), std::string("Ayoub")) << std::endl;
+			}
+			in_file.close();
+			out_file.close();
+		}
+		else
+			return error_file("Source File Error");
+	}
+	else
+		return error_file("Argument ERROR !");
+	return EXIT_SUCCESS;
+}
+
