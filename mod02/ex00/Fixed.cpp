11f9861727b5104e08e35b6e1fb2b38d3225d2dd
Ayoub Maatouch <aymaatou@e1r11p11.1337.ma>
2021-11-15 22:21:53 +0100

    ex00 mod02

--- mod02/ex00/Fixed.cpp
+++ mod02/ex00/Fixed.cpp
@@ -2,6 +2,37 @@
 
 Fixed::Fixed(void)
 {
+    std::cout<<"Default constructor called"<<std::endl;
     this->value = 0;
-    std::cout<<"Const called"<<std::endll;
+}   
+
+Fixed::Fixed(Fixed const &src )
+{
+    std::cout<<"Copy constructor called"<<std::endl;
+    *this = src;
+}   
+
+
+Fixed::~Fixed(void)
+{
+    std::cout<<"Destructor called"<<std::endl;
+}   
+
+int     Fixed::getRawBits(void ) const {
+
+    std::cout<<"getRawBits member function called"<<std::endl;
+    return (this->value);
+}
+
+void     Fixed::setRawBits(int const raw) {
+
+    std::cout<<"setRawBits member function called"<<std::endl;
+    this->value = raw;
+}
+
+Fixed&    Fixed::operator=(Fixed const &src) {
+
+    std::cout<<"Assignation operator called"<<std::endl;
+    this->value = src.getRawBits();
+    return *this;
 }
--- mod02/ex00/Fixed.hpp
+++ mod02/ex00/Fixed.hpp
@@ -1,24 +1,14 @@
 #include <iostream>
 
 class Fixed {
+    public:
+    Fixed();
+    Fixed(Fixed const &src);
+    ~Fixed();
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+    Fixed& operator=(Fixed const &src);
     private:
         int value;
         static int fra_bit;
-    public:
-    Fixed();
-    // Fixed();
-    ~Fixed();
-    // int getRawBits( void ) const;
-    // void setRawBits( int const raw );
-    void    fun(int value) {
-        std::cout<<value<<std::endl;
-    };
-    void    fun(char value) {
-        std::cout<<value<<std::endl;
-    };
-    void    fun(std::string value) {
-        std::cout<<value<<std::endl;
-    };
-
-
 };
