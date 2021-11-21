7989e3709e9f5dd8d5ff1ba96ba4b9fb67e0d04e
Ayoub Maatouch <aymaatou@e1r12p11.1337.ma>
2021-11-21 10:40:10 +0100

    minor changes

--- mod02/ex02/main.cpp
+++ mod02/ex02/main.cpp
@@ -6,12 +6,12 @@ int main( void ) {
     Fixed b( 10);
     
     std::cout << a << std::endl;
-    // std::cout << ++a << std::endl;
-    // std::cout << a << std::endl;
-    // std::cout << a++ << std::endl;
-    // std::cout << a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
     std::cout << b << std::endl;
-    // std::cout << Fixed::max( a, b ) << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
     
     std::cout << a * b <<std::endl;
     return 0;
